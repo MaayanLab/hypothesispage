fa462d9af91a26fa440ee95e97a966f6b6530184
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2023-01-19 15:03:16 -0500

    added download buttons to tables

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 docker rmi -f $(docker images | grep "^<none>" | awk "{print $3}")
 docker rm $(docker ps -q -f status=exited)
 
-docker build --platform linux/amd64 -f Dockerfile -t maayanlab/hype:1.19 .
+docker build --platform linux/amd64 -f Dockerfile -t maayanlab/hype:1.24 .
 
-docker push maayanlab/hype:1.19
+docker push maayanlab/hype:1.24
 #docker run -p 5557:5557 -it maayanlab/hype:1.11
 
 #docker run -v /Users/maayanlab/Documents/GitHub/hypothesispage/:/hype -p 5557:5557 -e ENDPOINT=hype -it maayanlab/hype:1.4
